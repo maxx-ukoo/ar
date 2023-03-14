ac947a20d783d3987b6b015c2d80029f668d0bb0
maxx <maxx@maxx.net.ua>
2023-03-14 11:09:51 +0200

    This change break run

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@ module ua.net.maxx.actionregister {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
-    //requires liquibase.core;
+    requires liquibase.core;
 
     opens ua.net.maxx.actionregister to javafx.graphics;
 
